004db26d91367b34c328e2e9bc438b1521e362ad
Robin Ferm <robinferm@live.com>
2020-12-21 21:27:05 +0100

    mongodb connection

---
+++ db.js
@@ -0,0 +1,44 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const dbname = "crud_mongodb";
+const url = "mongodb://localhost:27017";
+const mongoOptions = {useUnifiedTopology : true};
+
+// Object with a db property that will be set to a db instance
+const state = {
+    db : null
+}
+
+const connect = (cb) => {
+
+    // If there is a db connection
+    if (state.db) {
+        cb();
+    }
+    else {
+        MongoClient.connect(url, mongoOptions, (err, client) => {
+            if (err) {
+                cb(err);
+            }
+            else{
+                // Create new instance of dbname and set to "state.db"
+                state.db = client.db(dbname);
+                cb();
+            }
+        });
+    }
+}
+
+/* Convert _id string to an <ObjectID> object
+   "An ObjectId in MongoDB is a 12-byte BSON type" https://blog.eduonix.com/web-programming-tutorials/learn-structure-objectid-mongodb/
+*/
+ const getPrimaryKey = (_id) => {
+    return ObjectID(_id);
+}
+
+// Return the database instance
+const getDB = () => {
+    return state.db;
+}
+
+module.exports = {getDB, connect, getPrimaryKey};
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const path = require('path');
-
+const db = require('./db');
+const collection = "todo";
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+db.connect((err) => {
+    if (err) {
+        console.log('Unable to connect to database');
+        // Terminate nodejs process - https://nodejs.org/api/process.html#process_process_exit_code
+        process.exit(1);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Connected to database, listening at http://localhost:${port}`);
+        });
+    }
+});
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
-});
-
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
 });
