42a2aa042e4eb283107e26dcf3745dfa89776211
Robin Ferm <robinferm@live.com>
2020-12-28 22:11:33 +0100

    cleanup

--- public/script.js
+++ public/script.js
@@ -18,54 +18,17 @@ const getTodos = () => {
 }
 getTodos();
 
-// Example POST method implementation:
+// Create todo
 async function createTodo(url = '/', data = {todo : userInput.value}) {
-    // Default options are marked with *
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      //mode: 'no-cors', // no-cors, *cors, same-origin
-      //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      //credentials: 'same-origin', // include, *same-origin, omit
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      //redirect: 'follow', // manual, *follow, error
-      //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
+      body: JSON.stringify(data)
     }).then(response => {response.json()})
       .catch(err => {console.log(err)})
-   // return response.json(); // parses JSON response into native JavaScript objects
-  }
-  
-//   createTodo()
-//     .then(data => {
-//       console.log(data); // JSON data parsed by `data.json()` call
-//     });
-
-
-
-
-// Delete todo
-// async function deleteTodo(todo) {
-//   console.log('click1')
-//   button = getElementById('delete_5fe9ed217ffa305ff4037771')
-//   alert(button)
-//     const response = await fetch(`/${todo._id}`, {
-//       method: 'DELETE'
-//     }).then(response => {response.json()})
-//       //.then(`${todo._id}`.remove())
-//       .catch(err => {console.log(err)})
-// }
-
-// deleteTodo()
-
-
-// document.getElementsByTagName("button").addEventListener("click", function() {
-// alert(todo._id)
-// })
-
-
+}
 
 // Delete todo
 const deleteTodo = (clicked_id) => {
